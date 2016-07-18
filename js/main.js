5690f68d8f7e1f207751b79ac1934001b8dbf45b
Preston Jones <psmak4@gmail.com>
2016-07-18 01:03:16 -0600

    Began redesign and project structure.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+﻿require.config({
+	paths: {
+		jquery: 'lib/jquery/jquery.min'
+	}
+});
+
+require(['jquery'], function ($) {
+	$(function () {
+		$('.header__nav-toggle').click(function (event) {
+			event.preventDefault();
+			$('.header__nav').toggleClass('header__nav--show');
+		});
+
+		var header = $(".header");
+		$(window).scroll(function () {
+			var scroll = $(window).scrollTop();
+
+			if (scroll >= 50) {
+				header.addClass('header--has-background');
+			} else {
+				header.removeClass('header--has-background');
+			}
+		});
+	});
+});
